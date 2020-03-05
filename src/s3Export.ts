1decbf80eb09475c41750c950e319384f4dc5569
zainab <maleki68.net@gmail.com>
2020-03-05 10:19:50 +0800

    changed template.yaml to remove unused param

--- src/s3Export.ts
+++ src/s3Export.ts
@@ -1,5 +1,16 @@
 import { ScheduledEvent } from 'aws-lambda';
+// import { CloudWatch } from 'aws-sdk';
 
 export async function handler(event: ScheduledEvent): Promise<void> {
-  console.log('Hello World: ', event);
+  console.log('Event is: ', event);
+  // const cw = new CloudWatch();
+  // const x: CloudWatch.GetMetricStatisticsInput = {
+  //   MetricName: 'pipeline',
+  //   Namespace: '',
+  //   Period: 30,
+  //   StartTime: new Date(),
+  //   EndTime: new Date(),
+  // };
+  // const result = await cw.getMetricStatistics(x).promise;
+  // console.log('Hello World: ', result);
 }
